0b767d8b199361a156833a5b48354c5c1b9cf0ff
NormanJaimes <normane_@hotmail.com>
2024-08-08 16:02:43 -0500

    Agrupar funciones para el llamada de la API y obtener los datos

--- src/store.ts
+++ src/store.ts
@@ -1,31 +1,21 @@
 import { create } from 'zustand';
-import axios from 'axios';
-import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
 import { Cryptocurrency } from './types';
+import { devtools } from 'zustand/middleware';
+import { getCryptos } from './services/CryptoService';
 
 type CryptoStore = {
   cryptocurrencies: Cryptocurrency[];
   fetchCryptos: () => Promise<void>;
 };
 
-async function getCryptos() {
-  const url =
-    'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
-  const {
-    data: { Data },
-  } = await axios(url);
-  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-  if (result.success) {
-    return result.data;
-  }
-}
-
-export const useCryptoStore = create<CryptoStore>((set) => ({
-  cryptocurrencies: [],
-  fetchCryptos: async () => {
-    const cryptocurrencies = await getCryptos();
-    set(() => ({
-      cryptocurrencies,
-    }));
-  },
-}));
+export const useCryptoStore = create<CryptoStore>()(
+  devtools((set) => ({
+    cryptocurrencies: [],
+    fetchCryptos: async () => {
+      const cryptocurrencies = await getCryptos();
+      set(() => ({
+        cryptocurrencies,
+      }));
+    },
+  }))
+);
