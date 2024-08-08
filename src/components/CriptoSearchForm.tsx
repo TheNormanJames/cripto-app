a599518275cd9b95b92bc87050aee9e41e9c7f1f
NormanJaimes <normane_@hotmail.com>
2024-08-08 13:44:24 -0500

    Agregando Zod para validación de datos

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,3 +1,4 @@
+import { currencies } from '../data/index';
 export default function CriptoSearchForm() {
   return (
     <form className="form">
@@ -5,6 +6,11 @@ export default function CriptoSearchForm() {
         <label htmlFor="currency">Moneda</label>
         <select name="currency" id="currency">
           <option value="">-- Seleccione --</option>
+          {currencies.map((currency) => (
+            <option value={currency.code} key={currency.code}>
+              {currency.name}
+            </option>
+          ))}
         </select>
       </div>
       <div className="field">
