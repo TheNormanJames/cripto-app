e07577a1ffd7222853d8b38ff3f5ff47e4eeecfa
NormanJaimes <normane_@hotmail.com>
2024-08-08 14:40:32 -0500

    Agregando zustand

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,8 @@
 import { z } from 'zod';
-import { CurrencySchema } from '../schema/crypto-schema';
+import {
+  CurrencySchema,
+  CryptoCurrenciesResponseSchema,
+} from '../schema/crypto-schema';
 
 export type Currency = z.infer<typeof CurrencySchema>;
+export type Cryptocurrency = z.infer<typeof CryptoCurrenciesResponseSchema>;
